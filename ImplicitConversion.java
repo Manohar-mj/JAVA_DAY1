63475d84798e80441ff26084542221f244e7fff0
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-07-15 12:15:23 +0530

    Create ImplicitConversion.java

---
+++ ImplicitConversion.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        
+        double d = 65.4;
+        int i = (int)d;
+        char c = (char)i;
+        
+        System.out.println(i);
+        System.out.println(c);
+    }
+}
