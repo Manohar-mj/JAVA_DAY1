4ed277e143546912792df1a1f9f3bb64e43dbd74
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-07-15 12:13:13 +0530

    Create AutoBoxing.java

---
+++ AutoBoxing.java
@@ -0,0 +1,19 @@
+import java.util.*; 
+
+
+class AutoBoxing
+{ 
+    public static void main(String[] args) 
+    { 
+        char ch = 'a';
+
+        Character a = ch;
+
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+        arrayList.add(25);
+
+        System.out.println(arrayList.get(0));     
+    } 
+} 
+
