9b82ee4ad44af363dd71cfe5a7a55b1fa6bd0736
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-07-15 12:14:45 +0530

    Create ExplicitConversion.java

---
+++ ExplicitConversion.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        
+        int x = 100;
+        long y = x;
+        float z = y;
+        
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
