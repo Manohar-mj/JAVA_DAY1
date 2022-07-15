f09aac74f02da96a82f9ccbd766c7676e9203a4b
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-07-15 12:09:28 +0530

    Create Primitive.java

---
+++ Primitive.java
@@ -0,0 +1,19 @@
+import java.math.*;
+import java.io.*;
+import java.util.*;
+
+
+class Test
+{ 
+    public static void main(String[] args) 
+    { 
+        int x1 = 10;
+        int x2 = x1;
+
+        x2 = 20;
+
+        System.out.println(x1);
+        System.out.println(x2);
+    } 
+} 
+
